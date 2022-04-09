727a30829761c3cc50dc140a124e6a5cecfc71d6
unknown <mayurpawar8691@gmail.com>
2022-04-09 12:47:12 +0530

    Check two lines are equal or not

--- LineComparisonProblem.java
+++ LineComparisonProblem.java
@@ -1,23 +1,28 @@
-import java.util.Scanner;
-
 public class LineComparisonProblem {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        //first line co-ordinates
+        int x1 = 10, y1 = 20;
+        int x2 = 20, y2 = 40;
+        //second line co-ordinates
+        int x3 = 10, y3 = 40;
+        int x4 = 20, y4 = 60;
 
-        System.out.print("Enter First point Co-ordinates: ");
-        int x1 = scan.nextInt();
-        int y1 = scan.nextInt();
+        double lenght1 = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            System.out.println("Lenght of first Line= " + lenght1);
 
-        System.out.print("Enter Second point Co-ordinates: ");
-        int x2 = scan.nextInt();
-        int y2 = scan.nextInt();
+        double lenght2 = Math.sqrt(((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3)));
+            System.out.println("Lenght of second Line= " + lenght2);
 
-        double lenght = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-
-        System.out.println(lenght);
-
+        if (lenght1 == lenght2)
+            System.out.println("Two lines are equal");
+        
+        else {
+            System.out.println("Two lines are not equal");
+        }
+        
     }
 
 }
+
 
